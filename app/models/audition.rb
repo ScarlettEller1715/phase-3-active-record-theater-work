e99741b52c79877c57462a30351a9702f2115b97
Scarlett Eller <rq6715@gmail.com>
2022-02-28 15:55:23 -0500

    commit tree fe02e3d909d26bcfca2af564b34b39f720afb468

--- app/models/audition.rb
+++ app/models/audition.rb
@@ -1,3 +1,12 @@
 class Audition < ActiveRecord::Base
-  
+  belongs_to :role
+
+  def role
+    self.role
+  end
+
+  def call_back
+    call_back_status = self.role.hired
+    call_back_status.update(hired: true)
+  end
 end
